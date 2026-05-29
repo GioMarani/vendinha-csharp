8a6477b1a00ed32246217a802375b425b03a9cbd
Giovanni Marani <giovanni.pacaembu@gmail.com>
2026-05-29 20:02:05 -0300

    removido operacao de criar tabelas

--- Data/Database.cs
+++ Data/Database.cs
@@ -10,32 +10,5 @@ namespace VendinhaBackend.Data
         {
             return new SqliteConnection(connectionString);
         }
-
-        public void CriarTabelas()
-        {
-            using var connection = CriarConexao();
-            connection.Open();
-
-            using var command = connection.CreateCommand();
-            command.CommandText = @"
-                CREATE TABLE IF NOT EXISTS Clientes (
-                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    NomeCompleto TEXT NOT NULL,
-                    Cpf TEXT NOT NULL UNIQUE,
-                    DataNascimento TEXT NOT NULL,
-                    Email TEXT NULL
-                );
-
-                CREATE TABLE IF NOT EXISTS Dividas (
-                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    ClienteId INTEGER NOT NULL,
-                    Valor REAL NOT NULL,
-                    Situacao TEXT NOT NULL,
-                    DataCriacao TEXT NOT NULL,
-                    DataPagamento TEXT NULL,
-                    FOREIGN KEY (ClienteId) REFERENCES Clientes(Id) ON DELETE CASCADE
-                );";
-            command.ExecuteNonQuery();
-        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,12 +7,6 @@ builder.Services.AddSingleton<Database>();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var database = scope.ServiceProvider.GetRequiredService<Database>();
-    database.CriarTabelas();
-}
-
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
